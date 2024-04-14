b2961ddcdc3b4aa537237e77460d57eb43173bcf
Kirill <User@WIN-9507BK6FFVC>
2024-04-15 00:25:21 +0600

    Test thread work

--- ThreadTest/Program.cs
+++ ThreadTest/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+var thread = new Thread(WriteSecond);
+thread.Start();
+var a = 0;
+while(a < 1000)
+{
+    Console.WriteLine("Первый");
+    a++;
+}
+Console.ReadKey();
+
+static void WriteSecond()
+{
+    int b = 0;
+    while(b < 1000)
+    {
+        Console.WriteLine("         Второй");
+        b++;
+    }
+}
